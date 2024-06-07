42fc47a03d149dce2e4c9884c944fb590eb2ab69
atul2303 <atul.deshmukh.ad@gmail.com>
2024-06-06 21:34:38 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version='0.0.0',
+    author="Atul Deshmukh",
+    author_email="atul.deshmukh.ad@gmail.com",
+    packages=find_packages(),
+    install_requires=[])
